db5762c179297d930d5ae6baf3b1488cf3c1f847
Jan Berchtold <Jan.Berchtold@htwg-konstanz.de>
2019-10-16 14:30:36 +0200

    adds printf ad clock_realtime

--- chapter4/main.c
+++ chapter4/main.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <fcntl.h>
 #include <x86intrin.h>
+#include <time.h>
 //#include <w32api/lm.h>
 
 #define BILLION 1E9
@@ -99,7 +100,7 @@ void rdtsc () {
 }
 void clockRealtime () {
     struct timespec start, stop;
-    double accum;
+    double accum, mikro;
     /*
      * if (clock_gettime( CLOCK_REALTIME, &start) == -1) {
         perror("clock gettime");
@@ -114,9 +115,15 @@ void clockRealtime () {
     clock_gettime(CLOCK_REALTIME, &start);
     zeroByteRead();
     clock_gettime(CLOCK_REALTIME, &stop);
-    accum = ( stop.tv_sec - start.tv_sec )
-            + ( stop.tv_nsec - start.tv_nsec )
+
+    accum = ( (double)stop.tv_sec - (double)start.tv_sec )
+            + ( (double)stop.tv_nsec - (double)start.tv_nsec )
             / BILLION;
-    printf("%lf\n", accum);
+
+    mikro = (stop.tv_sec - start.tv_sec) *1000;
+    mikro += ((double)stop.tv_nsec - (double)start.tv_nsec) /100000;
+    mikro = mikro/1000;
+    accum = accum/1000;
+    printf("accum = %lf  millsec = %lf\n", accum, mikro);
     return;
 }
