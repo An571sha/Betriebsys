6def16f566daff345e0f54c043e8ccf66bbe6ce8
Animesh Sharma <anim_esh@yahoo.co.in>
2019-10-23 11:05:23 +0200

    ++ code cleanup ++

--- CpuMechanism/contextSwitchMeasure.c
+++ CpuMechanism/contextSwitchMeasure.c
@@ -23,6 +23,7 @@
 #include <string.h>
 
 int createTwoPipesAndMeasureContextSwitch();
+int timeSpentOnReadAndWrite();
 
 int main(int argc, char **argv) {
     createTwoPipesAndMeasureContextSwitch();
@@ -56,10 +57,7 @@ int createTwoPipesAndMeasureContextSwitch(){
         //parent process
     } else if (processId > 0) {
 
-        //wait for child process to write on array1ForPipe
-        //wait(NULL);
-
-       cpu_set_t mask;
+     cpu_set_t mask;
         CPU_ZERO(&mask);
         CPU_SET(3, &mask);
         if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) < 0)
@@ -71,37 +69,23 @@ int createTwoPipesAndMeasureContextSwitch(){
         //close the reading end of the first pipe
         close(array0ForPipe[0]);
 
-        //write on the writing end of first pipe
+        //write something on the writing end of first pipe
         write(array0ForPipe[1], fixed_str_1, (strlen(fixed_str_1)+1));
 
         //close the write of second pipe
         close(array1ForPipe[1]);
 
-        //get time stamp at this point
-        clock_gettime(CLOCK_MONOTONIC_RAW, &start);
-
-        //read from the reading end of second pipe
+        //read the time spent in Context Switch from the reading end of second pipe
         read(array1ForPipe[0], &input_long_2, sizeof(long));
 
-        // get time stamp after wait and child process are done
-        clock_gettime(CLOCK_MONOTONIC_RAW, &end);
-
-        elapsedTime1 = end.tv_nsec;
-
-        if (start.tv_nsec > end.tv_nsec) {
-            elapsedTime1 += ((long) end.tv_sec - (long) start.tv_sec) * 1000000000;
-        }
-
-        elapsedTime1 = (elapsedTime1 - start.tv_nsec);
-
-        totalTime = (elapsedTime1 + input_long_2)/2;
+        totalTime = input_long_2;
 
         printf("time required for context switch child to parent %ld ns \n",totalTime);
 
         //child process
-    } else {
+    } else if (processId == 0){
 
-       cpu_set_t mask;
+        cpu_set_t mask;
         CPU_ZERO(&mask);
         CPU_SET(3, &mask);
         if (sched_setaffinity(0, sizeof(cpu_set_t), &mask) < 0)
@@ -116,7 +100,7 @@ int createTwoPipesAndMeasureContextSwitch(){
         //get time stamp at this point
         clock_gettime(CLOCK_MONOTONIC_RAW, &start);
 
-        //read from the reading end of first pipe
+        //context switch occurs here as we start read from the reading end of first pipe
         read(array0ForPipe[0],input_str_1, sizeof(input_str_1));
 
         // get time stamp after wait and child process are done
@@ -141,4 +125,8 @@ int createTwoPipesAndMeasureContextSwitch(){
     }
 
     return 0;
+}
+
+int timeSpentOnReadAndWrite(){
+
 }
