f2a410989558743d620b7534163a33c9e8a2fa10
Jan Berchtold <Jan.Berchtold@htwg-konstanz.de>
2019-10-25 11:24:55 +0200

    adds secmentation fault

---
+++ chapter14/cmake-build-debug/CMakeFiles/chapter14.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter14.dir/main.c.o"
+  "chapter14.pdb"
+  "chapter14.exe"
+  "libchapter14.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chapter14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ chapter14/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int *c1 = NULL;
+    //dereferencing it
+    *c1 = 5;
+    printf("lolol%d", *c1);
+    return 0;
+}
