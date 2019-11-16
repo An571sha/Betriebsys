d3df4b056da6733b8fbdb296530194d990dadbb5
Animesh Sharma <anim_esh@yahoo.co.in>
2019-11-16 17:25:30 +0100

    ++ added assembler code for a simple c program ++

---
+++ pagingTLB/array.c
@@ -0,0 +1,14 @@
+//
+// Created by Animesh Sharma on 16.11.19.
+//
+
+/** generated assembler code with gcc -save-temps -fverbose-asm array.c for closer code inspection **/
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int array[1000];
+    for (int i = 0; i < 1000; i++) {
+        array[i] = 0;
+    }
+}
+
