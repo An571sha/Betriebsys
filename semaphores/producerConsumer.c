ce5cf767579e6b905014e9cf8e7070df6a950f35
Animesh Sharma <anim_esh@yahoo.co.in>
2019-12-26 18:36:02 +0100

    ++ added 2 threads and an extra sem_t semaphore to avoid deadlock ++

--- semaphores/producerConsumer.c
+++ semaphores/producerConsumer.c
@@ -1,53 +1,81 @@
 #include <stdio.h>
 #include <semaphore.h>
+#include <pthread.h>
 
-#define MAX 1
+#define MAX 10
 
 int buffer[MAX];
 int fill = 0;
 int use = 0;
-int loops = MAX;
+int counter = 0;
 sem_t full;
 sem_t empty;
+sem_t mutex;
+
+typedef struct wert_t{
+    int i;
+} wert;
+
 
 void put(int value) {
     buffer[fill] = value;
     fill = (fill + 1) % MAX;
+    printf("fill - %d\n", fill);
+    counter++;
 }
 
 int get() {
     int tmp = buffer[use];
     use = (use + 1) % MAX;
+    printf("use - %d\n", use);
+    counter--;
     return tmp;
 }
 
-void* producer(){
-    sem_wait(&empty);
-    put(5);
-    sem_post(&full);
-    return NULL;
-}
+void producer(int loops){
+    for (int i = 0; i < loops; i++) {
 
-void* consumer(){
-    int tmp = 0;
-    while (tmp != -1) {
-        sem_wait(&full);
-        tmp = get();
-        sem_post(&empty);
-        printf("%d\n", tmp);
+        sem_wait(&empty);
+        sem_wait(&mutex);
+        put(i);
+        sem_post(&mutex);
+        sem_post(&full);
+
     }
 
-    return NULL;
+}
+
+void consumer(int loops){
+    int tmp = 0;
+    for (int i = 0; i < loops; i++) {
+        sem_wait(&full);
+        sem_wait(&mutex);
+        tmp = get();
+        sem_post(&mutex);
+        sem_post(&empty);
+        printf("tmp - %d\n", tmp);
+    }
+}
+
+void* worker(void* args){
+    wert *ct = (wert *) args;
+    producer(ct ->i);
+    consumer(ct ->i);
+    return (void*) NULL;
 }
 
 int main() {
+    pthread_t p0,p1;
+
+    wert t1,t2;
+    t1.i = 10;
+    t2.i = 10;
+
     sem_init(&empty, 0, MAX);
     sem_init(&full, 0, 0);
-    producer();
-/*    for (int i = 0; i < loops; i++) {
 
-       }
-*/
-    consumer();
+    pthread_create(&p0, NULL, worker, &t1);
+    pthread_create(&p1, NULL, worker, &t2);
+
     return 0;
 }
